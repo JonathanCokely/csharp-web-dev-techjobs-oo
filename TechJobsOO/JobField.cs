bde7b8579719c591c94e780224b5954acd095ab2
Cokely <JC032926@cerner.net>
2020-11-03 08:24:14 -0600

    Added Equals() method to JobField class

--- TechJobsOO/JobField.cs
+++ TechJobsOO/JobField.cs
@@ -30,6 +30,11 @@ namespace TechJobsOO
             return Value;
         }
 
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
         public override int GetHashCode()
         {
             return HashCode.Combine(Id, Value);
